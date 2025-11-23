b2b325eee9b757ba09a3197ad38a827e5b26b4d4
ValNick12 <115808998+ValNick12@users.noreply.github.com>
2025-11-23 19:04:39 +0200

    changes

--- src/strategies/CarStrategy.java
+++ src/strategies/CarStrategy.java
@@ -2,8 +2,8 @@ package strategies;
 
 public class CarStrategy extends BaseStrategy {
 
-    private double fuelConsumption;
-    private double fuelCost;
+    private final double fuelConsumption;
+    private final double fuelCost = 2.93;
 
     public CarStrategy(double speed, double distance, double fuelConsumption) {
         super(speed, distance);
@@ -22,6 +22,6 @@ public class CarStrategy extends BaseStrategy {
 
     @Override
     public double calculateCost() {
-        return this.distance * (this.fuelConsumption / 100) * this.fuelCost;
+        return this.distance * (this.fuelConsumption / 100) * fuelCost;
     }
 }
